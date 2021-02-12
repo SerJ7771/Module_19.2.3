abc42dea0894f84c58ef6650d65c4f05ecd8982f
SerJ7771 <74389938+SerJ7771@users.noreply.github.com>
2021-02-12 15:47:32 +0600

    Update first_tests.py

--- pythonProject3/tests/first_tests.py
+++ pythonProject3/tests/first_tests.py
@@ -3,6 +3,9 @@ from app.calculator import Calculator
 class TestCalc:
     def setup(self):
         self.calc = Calculator
+        
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 3, 7) == 21
 
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 8, 2) == 4
