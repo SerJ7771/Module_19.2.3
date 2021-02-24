b499316a206f96e00f28aaefa45c10e554a54076
SerJ7771 <74389938+SerJ7771@users.noreply.github.com>
2021-02-24 20:32:23 +0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+valid_email = 'sers7777@gmail.com'
+valid_pass = 'q2w3e4r5'
+
+invalid_email = 'sers77@gmail.com'
+invalid_password = 'q2w3e4r'
+
+invalid_auth_key = 'ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae657'
